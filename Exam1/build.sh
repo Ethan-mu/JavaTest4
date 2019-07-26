c16144711f2ce440aa80c7acc8ecb9f8264d75f8
阿木木 <45324034+Ethan-mu@users.noreply.github.com>
2019-07-26 18:21:48 +0800

    Update build.sh

--- Exam1/build.sh
+++ Exam1/build.sh
@@ -2,4 +2,4 @@ chmod +x start.sh
 chmod +x curl.sh
 chmod +x stop.sh
 mvn package -Dmaven.test.skip=true
-cp ./target/exam1-0.0.1-SNAPSHOT.jar ./docker/java/
+mv ./target/exam1-0.0.1-SNAPSHOT.jar ./docker/java/
